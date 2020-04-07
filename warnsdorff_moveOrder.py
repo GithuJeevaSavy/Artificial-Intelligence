2058e37ccecbaea567a89762b48b27cc4f539ded
Snake Charmer <32595688+PiyushMankad@users.noreply.github.com>
2020-04-07 11:01:29 +0100

    [Algorithm] Warnsdorff

--- warnsdorff_moveOrder.py
+++ warnsdorff_moveOrder.py
@@ -1,4 +1,4 @@
-	import argparse
+import argparse
 import cv2
 from heapq import heappush, heappop
 import random
@@ -30,11 +30,17 @@ aparser.add_argument('--rows', type=int, default=8,
 	help="rows in a chessboard")
 aparser.add_argument('--cols', type=int, default=8, 
 	help="columns in a chessboard")
+aparser.add_argument('--initX',type=int, default=0,
+	help="initial column")
+aparser.add_argument('--initY',type=int, default=0,
+	help="initial row")
 args = vars(aparser.parse_args())
 
 chessX = args["cols"]	# X
 chessY = args["rows"]	# Y
 
+# chessX = 6
+# chessY = 6
 # init a chessboard with zeros
 chessboard = np.zeros((chessY,chessX),dtype=np.int32)
 
@@ -45,6 +51,9 @@ moveY = [1, 2, 2, 1, -1, -2, -2, -1]
 # init position of the knight
 initX = random.randint(0,chessX-1)
 initY = random.randint(0,chessY-1)
+
+initX = args["initX"]
+initY = args["initY"]
 
 for step in range(chessX*chessY):
 	# print("step",step)
@@ -58,14 +67,20 @@ for step in range(chessX*chessY):
 
 		if isValid(newX,newY,chessboard):
 			degreeCount = getPossibleMoves(newX,newY,chessboard)
+			print("degrees {} and move {},{}".format(degreeCount, newX, newY))
 			heappush(priorityQueue,(degreeCount,i))
 
 	# pop the smallest element from the priority queue
+
 	if len(priorityQueue) > 0:
+		print("queue",step+1,priorityQueue)
 		(degree,moveNumber) = heappop(priorityQueue)
 		initX = initX + moveX[moveNumber]
 		initY = initY + moveY[moveNumber]
-	else: 
+		print("move number",moveX[moveNumber],moveY[moveNumber])
+		print("next move here",initX,initY)
+		print(chessboard)
+	else:
 		print("queue was empty")
 		break
 
